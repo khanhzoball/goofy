8049c61df41cc131c684de741987c0076c7b646c
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-03-22 21:21:05 -0700

    added voice join

--- goofy.py
+++ goofy.py
@@ -18,6 +18,15 @@ async def on_message(message):
     if message.content.startswith('goofy'):
         await message.channel.send('GOOFY HAS ARRIVED FOR THE RECKONING')
 
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.voice_channel
+        await client.join_voice_channel
+    else:
+        await ctx.send('GOOFY NEEDS VOICE CHANNEL')
+
+
 print(os.getenv('TOKEN'))
 
 client.run(os.getenv('TOKEN'))
